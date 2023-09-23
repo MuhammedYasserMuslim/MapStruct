7e82edd1d16a2cc1e370a5506ddaae00dbc91c86
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-09-23 22:57:16 +0300

    Add-Doctor-Repository

--- src/main/java/com/spring/controller/DoctorController.java
+++ src/main/java/com/spring/controller/DoctorController.java
@@ -3,11 +3,10 @@ package com.spring.controller;
 import com.spring.model.Doctor;
 import com.spring.services.DoctorServices;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/doctor")
@@ -17,9 +16,37 @@ public class DoctorController {
 
     private final DoctorServices doctorServices;
 
+    @GetMapping("/count")
+    public Long count() {
+        return doctorServices.count();
+    }
+
 
     @GetMapping("/doctors")
     public List<Doctor> findAll(){
         return doctorServices.findAll();
+    }
+    @GetMapping("/doctor")
+    public Optional<Doctor> findById(@RequestParam Long id) {
+        return doctorServices.findById(id);
+    }
+
+    @PostMapping("/doctor")
+    public Doctor insert(Doctor doctor) {
+        doctorServices.insert(doctor);
+        return doctor;
+    }
+
+    @PutMapping("/doctor")
+    public Doctor update(Doctor doctor) {
+        doctorServices.update(doctor);
+        return doctor;
+    }
+
+    @DeleteMapping("/doctors")
+    public Optional<Doctor> deleteById(@RequestParam Long id) {
+        doctorServices.deleteById(id);
+        return findById(id);
+
     }
 }
